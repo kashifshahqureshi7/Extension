51f796bacdd44cfcd64b2e9360ad25529415f7e9
kashifshahqureshi7 <kashifshahqureshi7@gmail.com>
2024-10-13 23:40:10 +0500

    initial commit

---
+++ ConsoleApp1/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Extension
+    {
+        public static int ToInt(this string value)
+        {
+            return Convert.ToInt32(value);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string year = "2024";
+            int year2 = year.ToInt() + 1;
+
+            Console.WriteLine(year);
+            Console.WriteLine(year2);
+        }
+    }
+}
